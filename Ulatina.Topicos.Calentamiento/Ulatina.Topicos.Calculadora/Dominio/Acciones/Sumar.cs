2cfd07af89296e46e3c84812f9bfedbc671d7e12
laboratorio <laboratorio@LAB09-18>
2017-01-28 16:27:59 -0600

    Calculadora

---
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.Calculadora/Dominio/Acciones/Sumar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.Topicos.Calculadora.Dominio.Acciones
+{
+    public class Sumar
+    {
+
+    }
+}
